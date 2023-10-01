fafdc8c3d6e7fce8a93845c6c36f4f50fb289b60
HoangAnh31156 <anhhvph31156@fpt.edu.vn>
2023-10-01 21:47:29 +0700

    Update

--- TrainingCSharp3_BT2/Views/FrmSach.cs
+++ TrainingCSharp3_BT2/Views/FrmSach.cs
@@ -14,45 +14,16 @@ namespace TrainingCSharp3_BT2.Views
 {
     public partial class FrmSach : Form
     {
-        SachService _service;
-        List<Guid> _lstIdSach = new List<Guid>();
-        Guid _idWhenClick;
+       
         public FrmSach()
         {
             InitializeComponent();
-            _service = new SachService();
+            
         }
         private void FrmSach_Load(object sender, EventArgs e)
         {
-            LoadCombobox();
-            LoadGrid(null);
+            
         }
-        public void LoadCombobox()
-        {
-            foreach (var item in _service.GetNxbs())
-            {
-                _lstIdSach.Add(item.Id);
-                cmbNXB.Items.Add(item.Ten);
-            }
-            cmbNXB.SelectedIndex = 0;
-        }
-        public void LoadGrid(string input)
-        {
-            int stt = 1;
-            dgvDSSach.ColumnCount = 5;
-            dgvDSSach.AutoSizeColumnsMode  = DataGridViewAutoSizeColumnsMode.Fill;
-            dgvDSSach.Columns[0].Name = "ID";
-            dgvDSSach.Columns[0].Visible = false;
-            dgvDSSach.Columns[1].Name = "STT";
-            dgvDSSach.Columns[2].Name = "Mã sách";
-            dgvDSSach.Columns[3].Name = "Tên sách";
-            dgvDSSach.Columns[4].Name = "Nhà xuất bản";
-            dgvDSSach.Rows.Clear();
-            foreach (var item in _service.GetSaches(input))
-            {
-                var NXB = _service.GetNxbs().FirstOrDefault(x => x.Id == item.IdNxb);
-                dgvDSSach.Rows.Add(item.Id, stt++, item.Ma, item.Ten, (NXB.Ten == null ? "Sách Fa Ke" : NXB.Ten));
-            }
-        }
+       
     }
 }
